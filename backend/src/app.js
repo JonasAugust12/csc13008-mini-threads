3a59fd5074eca6a8a35a2f88cc8efbd47df4e260
hohanhi <90816483+sleepynhiho@users.noreply.github.com>
2024-11-01 05:09:28 +0700

    build: install dependencies for ejs and add backend structure

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+// const searchRoutes = require('./routes/searchRoutes');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(ejsLayouts); 
+
+app.use(express.static(path.join(__dirname, '../../frontend/src')));
+
+// // Cài đặt route
+// app.use('/search', searchRoutes);
+
+// Route chính để hiển thị layout
+app.get('/', (req, res) => {
+    res.render('layout', {
+      title: 'Threads',
+      body: '',
+      cssFile: 'style.css'
+  });
+});
+
+// Khởi động server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
