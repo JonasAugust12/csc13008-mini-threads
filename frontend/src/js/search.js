37ccea7038faef6c65a0fa7643d2e88703a7f726
hohanhi <90816483+sleepynhiho@users.noreply.github.com>
2024-11-06 21:08:56 +0700

    fix: fix search result screen

--- frontend/src/js/search.js
+++ frontend/src/js/search.js
@@ -1,65 +1,65 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const searchInput = $(".search-box__input");
-const userProfiles = $$(".user-profile");
-const suggestionHeading = $(".suggestion-box__heading")
-const clearButton = $(".search-box__clear-btn");
+const searchInput = $('.search-box__input');
+const userProfiles = $$('.user-profile');
+const suggestionHeading = $('.suggestion-box__heading');
+const clearButton = $('.search-box__clear-btn');
 
-searchInput.addEventListener("input", function() {
+searchInput.addEventListener('input', function () {
     const query = this.value.trim().toLowerCase();
     if (query.length > 0) {
-        suggestionHeading.style.display = "none"
+        suggestionHeading.style.display = 'none';
+    } else {
+        suggestionHeading.style.display = 'flex';
     }
-    else {
-        suggestionHeading.style.display = "flex"
-    }
-    
-    userProfiles.forEach(profile => {
-        const username = profile.querySelector(".user-profile__username-link").textContent.toLowerCase();
-        const name = profile.querySelector(".user-profile__name-text").textContent.toLowerCase();
+
+    userProfiles.forEach((profile) => {
+        const username = profile.querySelector('.user-profile__username-link').textContent.toLowerCase();
+        const name = profile.querySelector('.user-profile__name-text').textContent.toLowerCase();
 
         if (username.includes(query) || name.includes(query)) {
-            profile.style.display = "flex";
-        }
-        else {
-            profile.style.display = "none";
+            profile.style.display = 'flex';
+        } else {
+            profile.style.display = 'none';
         }
     });
 });
 
-clearButton.onclick = function() {
-    searchInput.value = "";
+clearButton.onclick = function () {
+    searchInput.value = '';
 
-    userProfiles.forEach(profile => {
-        profile.style.display = "flex";
-    })
-}
+    userProfiles.forEach((profile) => {
+        profile.style.display = 'flex';
+    });
+};
 
+document.addEventListener('DOMContentLoaded', () => {
+    const followers = $$('.user-profile__followers-count');
 
-document.addEventListener("DOMContentLoaded", () => {
-    const followers = $$(".user-profile__followers-count");
-
-    followers.forEach(count => {
+    followers.forEach((count) => {
         const numFollowers = parseInt(count.textContent);
         count.textContent = formatFollowers(numFollowers) + ' followers';
     });
-})
+});
 
 function formatFollowers(followers) {
     if (followers >= 1_000_000) {
         return (followers / 1_000_000).toFixed(1) + 'M';
-    }
-    else if (followers >= 100_000) {
+    } else if (followers >= 100_000) {
         return Math.floor(followers / 1000) + 'K';
-    } 
-    else if (followers >= 10_000) {
+    } else if (followers >= 10_000) {
         return (followers / 1000).toFixed + 'K';
-    }
-    else if (followers >= 1000) {
+    } else if (followers >= 1000) {
         return followers.toLocaleString();
-    }
-    else {
+    } else {
         return followers.toString();
     }
 }
+
+searchInput.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        localStorage.setItem('searchValue', searchInput.value);
+        window.location.href = 'search_result.html';
+    }
+});
