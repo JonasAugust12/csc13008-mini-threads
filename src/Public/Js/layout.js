8160b0f14ecc2f927d8d9ce12c5b24cc6375f2e8
JonasAugust12 <111167499+JonasAugust12@users.noreply.github.com>
2024-11-15 09:12:55 +0700

    feat: add create post modal

--- src/Public/Js/layout.js
+++ src/Public/Js/layout.js
@@ -15,3 +15,30 @@ function adjustHeadingWidth() {
         headingSection.style.minWidth = '';
     }
 }
+
+// Lấy các phần tử cần dùng
+const createModal = document.querySelector('.create-post-modal');
+const overlay = document.querySelector('.overlay');
+const createBtn = document.querySelector('.create-btn');
+const navUtilityCreate = document.querySelector('.nav-utility-create');
+const cancelBtn = document.querySelector('.create-post__header__cancel-btn');
+
+// Hàm hiển thị modal và overlay
+function showModal() {
+    createModal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+// Hàm ẩn modal và overlay
+function hideModal() {
+    createModal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+// Thêm sự kiện click để hiển thị modal khi bấm vào nav-utility-create hoặc create-btn
+createBtn.addEventListener('click', showModal);
+navUtilityCreate.addEventListener('click', showModal);
+
+// Thêm sự kiện click để ẩn modal khi bấm vào overlay hoặc cancelBtn
+overlay.addEventListener('click', hideModal);
+cancelBtn.addEventListener('click', hideModal);
