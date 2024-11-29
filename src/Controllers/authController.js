34882bd814e180c642e732111e38f181595e2888
Diendien1801 <116612379+Diendien1801@users.noreply.github.com>
2024-11-30 00:57:41 +0700

    feat: create login and signup feature and update some UI

--- src/Controllers/authController.js
+++ src/Controllers/authController.js
@@ -1,12 +1,158 @@
-const loginController = (req, res) => {
-    res.render('login', { layout: false });
+const bcrypt = require("bcrypt"); // hash password
+const User = require("../Models/User"); // model user
+const jwt = require("jsonwebtoken"); // token
+
+// GENERATE ACCESS TOKEN
+generateAccessToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+    },
+    // key bí mật
+    process.env.JWT_ACCESS_KEY,
+    // hạn sử dụng token
+    { expiresIn: "1h" }
+  );
+};
+// GENERATE REFRESH TOKEN
+
+generateRefreshToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+    },
+    // key bí mật
+    process.env.JWT_REFRESH_KEY,
+    // hạn sử dụng token
+    { expiresIn: "7d" }
+  );
+};
+// ĐĂNG NHẬP
+const loginController = async (req, res) => {
+  try {
+    // tìm user trong db
+
+    const user = await User.findOne({
+      $or: [{ username: req.body.username }, { email: req.body.username }],
+    });
+    console.log(user);
+    // nếu không tìm thấy
+    if (!user) {
+      return res.status(404).json("Wrong username");
+    }
+
+    // so sánh password
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    // nếu sai password
+    if (!validPassword) {
+      return res.status(400).json("Wrong password");
+    }
+    // nếu đúng cả username và password
+    if (user && validPassword) {
+      // tạo token
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
+
+      // lưu trữ token
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: false, // deploy chuyển thành true
+        path: "/",
+        // ngăn chặn CSRF
+        sameSite: "strict",
+      });
+
+      const { password, ...other } = user._doc;
+      res.status(200).json({ ...other, accessToken });
+    }
+  } catch (err) {
+    console.error("Error during login:", err);
+    res.status(500).json(err);
+  }
 };
 
-const signupController = (req, res) => {
-    res.render('signup', { layout: false });
+// ĐĂNG KÝ
+const signupController = async (req, res) => {
+  try {
+    const { username, email, password, fullname } = req.body;
+
+    // Kiểm tra xem các trường bắt buộc đã được nhập chưa
+    if (!username || !email || !password || !fullname) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(req.body.password, salt);
+
+    // tạo user mới
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashed,
+      fullname: req.body.fullname,
+    });
+
+    // lưu vào db
+    const user = await newUser.save();
+
+    // trả về kết quả
+    res.status(200).json(user);
+  } catch (err) {
+    console.error("Error during signup:", err);
+    res.status(500).json(err);
+  }
+};
+
+//REFRESH TOKEN
+requestRefreshToken = async (req, res) => {
+  // Lấy refreshToken từ cookie
+  const refreshToken = req.cookies.refreshToken;
+
+  // Nếu không có refreshToken (chưa đăng nhập,...)
+  if (!refreshToken) return res.status(401).json("You are not authenticated");
+
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY, (err, user) => {
+    if (err) return res.status(403).json("Token is not valid");
+
+    // không lỗi -> tạo token mới
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    // lưu vào cookie
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: false, // deploy chuyển thành true
+      path: "/",
+      // ngăn chặn CSRF
+      sameSite: "strict",
+    });
+
+    // trả về token mới
+    res.status(200).json({ accessToken: newAccessToken });
+  });
+};
+
+// LOGOUT
+const logoutController = async (req, res) => {
+  // xóa cookie refreshToken
+  res.clearCookie("refreshToken");
+
+  // xóa refreshToken trong mảng để ngăn chặn việc sử dụng token cũ
+  refreshTokens = refreshTokens.filter(
+    (token) => token !== req.cookies.refreshToken
+  );
+
+  // trả về kết quả
+  res.status(200).json("Logged out successfully");
 };
 
 module.exports = {
-    loginController,
-    signupController,
+  loginController,
+  signupController,
+  requestRefreshToken,
+  logoutController,
 };
--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 // src/app.js
-const express = require('express');
-const path = require('path');
-const ejsLayouts = require('express-ejs-layouts');
-const session = require('express-session');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const path = require("path");
+const ejsLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 // Kết nối database -> thành công -> print success
 dotenv.config();
 
 const connectToMongo = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-        // thành công
-        console.log('SUCCESSFULLY CONNECTED TO MONGO DB');
-    } catch (error) {
-        console.error('FAILED TO CONNECT TO MONGO DB', error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    // thành công
+    console.log("SUCCESSFULLY CONNECTED TO MONGO DB");
+  } catch (error) {
+    console.error("FAILED TO CONNECT TO MONGO DB", error);
+  }
 };
 
 connectToMongo();
@@ -25,37 +25,44 @@ connectToMongo();
 connectToMongo();
 const app = express();
 // Import các routes
-const searchRoutes = require('./Routes/searchRoutes');
-const homeRoutes = require('./Routes/homeRoutes');
-const activityRoutes = require('./Routes/activityRoutes');
-const authRoutes = require('./Routes/authRoutes');
-const profileRoutes = require('./Routes/profileRoutes');
-const postRoutes = require('./Routes/postRoutes');
+const searchRoutes = require("./Routes/searchRoutes");
+const homeRoutes = require("./Routes/homeRoutes");
+const activityRoutes = require("./Routes/activityRoutes");
+const authRoutes = require("./Routes/authRoutes");
+const profileRoutes = require("./Routes/profileRoutes");
+const postRoutes = require("./Routes/postRoutes");
 
 // Cài đặt view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middleware session
 app.use(
-    session({
-        secret: 'your-secret-key',
-        resave: false,
-        saveUninitialized: true,
-    }),
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 app.use(ejsLayouts);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, './Public')));
+app.use(express.static(path.join(__dirname, "./Public")));
+
+app.use(cookieParser());
 
 // Cài đặt các routes
-app.use('/search', searchRoutes);
-app.use('/', homeRoutes); // Đường dẫn '/' cho trang home
-app.use('/activity', activityRoutes);
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-app.use('/post', postRoutes);
+app.use("/search", searchRoutes);
+app.use("/", homeRoutes); // Đường dẫn '/' cho trang home
+app.use("/activity", activityRoutes);
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+app.use("/post", postRoutes);
+app.get("/go-to-signup", (req, res) => {
+  res.redirect("/auth/signup");
+});
 // // Route chính để hiển thị layout
 // app.get('/', (req, res) => {
 //     const username = req.session.username || 'Guest';
@@ -72,5 +79,5 @@ app.use('/post', postRoutes);
 // Khởi động server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
